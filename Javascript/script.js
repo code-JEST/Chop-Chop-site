3ecb9f2ba659b287d5f1281090bdf38ed0a3fbb1
Adrian H <adrianhoff@gmail.com>
2022-12-03 18:52:24 +0100

    add slideshow

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,3 +1,4 @@
+ /*MENY*/
 let menu = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
@@ -6,7 +7,24 @@ menu.onclick = () => {
     navbar.classList.toggle('open');
 }
 
+ /*darkmode*/
 function darkmode() {
     var element = document.body;
     element.classList.toggle("dark-mode");
+ }
+
+ /*slideshow*/
+ let slideIndex = 0;
+ showSlides();
+ 
+ function showSlides() {
+   let i;
+   let slides = document.getElementsByClassName("Picture-slide");
+   for (i = 0; i < slides.length; i++) {
+     slides[i].style.display = "none";
+   }
+   slideIndex++;
+   if (slideIndex > slides.length) {slideIndex = 1}
+   slides[slideIndex-1].style.display = "block";
+   setTimeout(showSlides, 4000);
  }
